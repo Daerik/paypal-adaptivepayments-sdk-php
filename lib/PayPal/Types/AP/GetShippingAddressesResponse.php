98381f812a8a22adf3d9c5bc46831636bf1ffe8a
Daerik <8672541+Daerik@users.noreply.github.com>
2024-09-24 18:41:54 -0400

    Nullable properties

--- lib/PayPal/Types/AP/GetShippingAddressesResponse.php
+++ lib/PayPal/Types/AP/GetShippingAddressesResponse.php
@@ -4,33 +4,13 @@
 	use PayPal\Core\PPMessage;
 	use PayPal\Types\Common\ErrorData;
 	use PayPal\Types\Common\ResponseEnvelope;
+	
 	/**
 	 * The response to get the shipping addresses available for a
 	 * payment.
 	 */
-	class GetShippingAddressesResponse
-		extends PPMessage {
-		
-		/**
-		 *
-		 * @access public
-		 * @var ResponseEnvelope
-		 */
-		public ResponseEnvelope $responseEnvelope;
-		
-		/**
-		 *
-		 * @access public
-		 * @var Address
-		 */
-		public Address $selectedAddress;
-		
-		/**
-		 *
-		 * @array
-		 * @access public
-		 * @var ErrorData
-		 */
-		public ErrorData $error;
-		
+	class GetShippingAddressesResponse extends PPMessage {
+		public ?ResponseEnvelope $responseEnvelope = NULL;
+		public Address           $selectedAddress;
+		public ErrorData         $error;
 	}
