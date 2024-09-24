37581fcf0617b59967b779d3209e9a355ccfa10e
Daerik <8672541+Daerik@users.noreply.github.com>
2024-09-24 18:23:23 -0400

    Nullable properties

--- lib/PayPal/Types/AP/SetPaymentOptionsResponse.php
+++ lib/PayPal/Types/AP/SetPaymentOptionsResponse.php
@@ -4,25 +4,11 @@
 	use PayPal\Core\PPMessage;
 	use PayPal\Types\Common\ErrorData;
 	use PayPal\Types\Common\ResponseEnvelope;
+	
 	/**
 	 * The response message for the SetPaymentOption request
 	 */
-	class SetPaymentOptionsResponse
-		extends PPMessage {
-		
-		/**
-		 *
-		 * @access public
-		 * @var ResponseEnvelope
-		 */
-		public ResponseEnvelope $responseEnvelope;
-		
-		/**
-		 *
-		 * @array
-		 * @access public
-		 * @var ErrorData
-		 */
-		public ErrorData $error;
-		
+	class SetPaymentOptionsResponse extends PPMessage {
+		public ?ResponseEnvelope $responseEnvelope = NULL;
+		public ErrorData         $error;
 	}
