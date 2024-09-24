3c73c4a471e00dcd1f24f5237e1c38ac9310aa1c
Daerik <8672541+Daerik@users.noreply.github.com>
2024-09-24 18:16:00 -0400

    Nullable properties

--- lib/PayPal/Types/AP/SetPaymentOptionsRequest.php
+++ lib/PayPal/Types/AP/SetPaymentOptionsRequest.php
@@ -3,61 +3,19 @@
 	
 	use PayPal\Core\PPMessage;
 	use PayPal\Types\Common\RequestEnvelope;
+	
 	/**
 	 * The request to set the options of a payment request.
 	 */
 	class SetPaymentOptionsRequest
 		extends PPMessage {
-		
-		/**
-		 *
-		 * @access public
-		 * @var RequestEnvelope
-		 */
-		public RequestEnvelope $requestEnvelope;
-		
-		/**
-		 *
-		 * @access public
-		 * @var string
-		 */
-		public mixed $payKey;
-		
-		/**
-		 *
-		 * @access public
-		 * @var InitiatingEntity
-		 */
+		public ?RequestEnvelope $requestEnvelope;
+		public ?string          $payKey;
 		public InitiatingEntity $initiatingEntity;
-		
-		/**
-		 *
-		 * @access public
-		 * @var DisplayOptions
-		 */
-		public DisplayOptions $displayOptions;
-		
-		/**
-		 *
-		 * @access public
-		 * @var string
-		 */
-		public string $shippingAddressId;
-		
-		/**
-		 *
-		 * @access public
-		 * @var SenderOptions
-		 */
-		public SenderOptions $senderOptions;
-		
-		/**
-		 *
-		 * @array
-		 * @access public
-		 * @var ReceiverOptions
-		 */
-		public ReceiverOptions $receiverOptions;
+		public DisplayOptions   $displayOptions;
+		public string           $shippingAddressId;
+		public SenderOptions    $senderOptions;
+		public ReceiverOptions  $receiverOptions;
 		
 		/**
 		 * Constructor with arguments
